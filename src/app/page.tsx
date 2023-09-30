b9f3b83085d638c5ce42e932bdff43a756d42bc7
Alessandro Janicki <alessandro.janicki@ef.com>
2023-09-29 21:10:53 -0300

    feat: background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,30 @@
-import Image from 'next/image'
+import Button from "@/components/Button";
+import Link from "next/link";
+import "@fontsource/audiowide";
 
 export default function Home() {
   return (
-    <main>
-      
+    <main className="bg-[url('../../public/img/bg-lp.png')] bg-cover w-screen h-screen">
+      <div>
+        <div className="min-w-full min-h-full grid-cols-2">
+          <div className="flex-col">
+            <h1>
+              E-WASTE
+            </h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sed
+              erat purus. Proin consequat tellus non neque mattis luctus.
+              Pellentesque at purus in elit gravida ullamcorper. Curabitur non
+              tristique lectus. Ut et ex id metus malesuada ullamcorper.{" "}
+            </p>
+          </div>
+          <div>
+            <Link href={"/deposit"}>
+              <Button color="primary">DEPOSITAR</Button>
+            </Link>
+          </div>
+        </div>
+      </div>
     </main>
-  )
+  );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,12 +8,15 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        'primary-500': '#04B545',
+        'lightGrey-500': '#d4d4d4',
+        'darkGrey-500': '#d9d9d9',
       },
-    },
+      fontFamily: {
+        'Astigmatic': ['Audiowide']
+      }
+    }
   },
   plugins: [],
 }

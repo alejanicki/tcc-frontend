e16a4d143bcac9a881f2885a04c2aea3dfca0d7e
Alessandro Janicki <alessandro.janicki@ef.com>
2023-10-02 16:58:09 -0300

    feat: functional menu

--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -1,8 +1,11 @@
 "use client";
 import { useState } from "react";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
 
 export default function Menu() {
   const [isOpen, setIsOpen] = useState(false);
+  const pathname = usePathname();
 
   const handleClick = () => {
     setIsOpen(!isOpen);
@@ -11,7 +14,7 @@ export default function Menu() {
   return (
     <main>
       <div
-        className="bg-darkGrey-500 w-14 h-14 rounded-xl ml-4 mt-4 flex flex-col justify-center items-center border-2 border-primary-500 fixed"
+        className="bg-darkGrey-500 w-14 h-14 rounded-xl ml-4 mt-4 flex flex-col justify-center items-center border-2 border-primary-500 fixed z-10"
         onClick={handleClick}
       >
         <div>
@@ -33,20 +36,41 @@ export default function Menu() {
         </div>
       </div>
       <div
-        className={`bg-white h-screen w-4/5 rounded-lg flex flex-col${
-          isOpen ? "w-3/4" : "w-0"
+        className={`bg-white h-screen w-4/5 rounded-lg flex flex-col transition-all duration-300 ease-in ${
+          isOpen ? "w-0" : "-translate-x-full"
         }`}
       >
-        <div className="m-auto text-primary-500 text-center">
-            <div className="h-10 mb-2">
-                <h1>Meu Perfil</h1>
-            </div>
-            <div className="h-10 mb-2">
-                <h1>Depositar</h1>
-            </div>
-            <div className="h-10">
-                <h1>Trocar Pontos</h1>
-            </div>
+        <div className="flex flex-col h-1/4 w-3/4 text-center justify-between m-auto text-primary-500 relative">
+          <Link
+            href={"/profile"}
+            className={`h-10 pt-2 ${
+              pathname == "/profile"
+                ? "bg-primary-500 rounded-xl text-white"
+                : "bg-transparent"
+            }`}
+          >
+            <h1>Meu Perfil</h1>
+          </Link>
+          <Link
+            href={"/deposit"}
+            className={`h-10 pt-2 ${
+              pathname == "/deposit"
+                ? "bg-primary-500 rounded-xl text-white"
+                : "bg-transparent"
+            }`}
+          >
+            <h1>Depositar</h1>
+          </Link>
+          <Link
+            href={"/points"}
+            className={`h-10 pt-2 ${
+              pathname == "/profile"
+                ? "bg-primary-500 rounded-xl text-white"
+                : "bg-transparent"
+            }`}
+          >
+            <h1>Trocar Pontos</h1>
+          </Link>
         </div>
       </div>
     </main>

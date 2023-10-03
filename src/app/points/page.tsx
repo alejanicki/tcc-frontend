4dd63dddc4e8b0c283b22d1fa1291f7510271a1e
Alessandro Janicki <alessandro.janicki@ef.com>
2023-10-03 16:22:28 -0300

    points page init

--- src/app/points/page.tsx
+++ src/app/points/page.tsx
@@ -1,7 +1,20 @@
+import "@fontsource/audiowide";
+
 export default function Points() {
     return(
-        <div>
-            <h1>TESTE</h1>
-        </div>
+        <main className="bg-darkGrey-500 h-screen w-screen py-10">
+            <div className="bg-white h-full w-4/5 mx-auto rounded-xl pt-6">
+                <div className="w-full text-center text-primary-500 text-xl font-audiowide">
+                    <h1>Escolha o seu item</h1>
+                </div>
+                <div>
+                    <ul>
+                        <li><h1>ITEM</h1></li>
+                        <li><h1>ITEM</h1></li>
+                        <li><h1>ITEM</h1></li>
+                    </ul>
+                </div>
+            </div>
+        </main>
     )
 }
